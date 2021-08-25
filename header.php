37308cfeca9b6f64ccd22531c6bda7bf7d393d4f
Charles Harvey <harvey.charles@gmail.com>
2021-08-25 10:22:03 +0100

    add meta properties for social in header

--- header.php
+++ header.php
@@ -29,6 +29,32 @@
 	<meta name="description" content="<?php bloginfo('description'); ?>">
 	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 	<?php wp_head(); ?>
+
+
+	<?php $smp_title = get_the_title(); ?>
+	<?php $smp_description = get_field('social_description'); ?>
+	<?php $smp_image = get_field('title_background_image'); ?>
+	<meta property="og:type" content="article" />
+	<meta property="og:site_name" content="<?php echo $smp_title; ?>" />
+	<meta property="og:title" content="<?php echo $smp_title; ?>">
+	<meta name="twitter:title" content="<?php echo $smp_title; ?>">
+	<meta itemprop="name" content="<?php echo $smp_title; ?>">
+
+	<?php if ($smp_description) : ?>
+		<meta name="twitter:card" value="<?php echo $smp_description; ?>">
+		<meta name="twitter:description" content="<?php echo $smp_description; ?>">
+		<meta property="og:description" content="<?php echo $smp_description; ?>">
+		<meta itemprop="description" content="<?php echo $smp_description; ?>">
+	<?php endif; ?>
+	<?php if ($smp_images) : ?>
+		<meta itemprop="image" content="<?php echo $smp_image; ?>">
+		<meta name="twitter:image" content="<?php echo $smp_image; ?>">
+		<meta property="og:image" content="<?php echo $smp_image; ?>">
+		<meta property="og:img" content="<?php echo $smp_image; ?>">
+	<?php endif; ?>
+
+
+
 </head>
 
 
