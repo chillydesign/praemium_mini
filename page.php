20c0b355136855bb4377c1980dbea57f8684bc38
Charles Harvey <harvey.charles@gmail.com>
2022-05-26 14:47:54 +0100

    blha

--- page.php
+++ page.php
@@ -7,7 +7,6 @@
 
 
 
-
 <!--		<?php // if (have_posts()): while (have_posts()) : the_post(); 
 			?>
  				<h2><?php the_title(); ?></h2>
--- section-loop.php
+++ section-loop.php
@@ -34,9 +34,7 @@
 			<?php get_template_part('navigation'); ?>
 		</div>
 
-		<div id="mobile_video">
-			<video playsinline muted loop poster="<?php echo get_field('title_background_image'); ?>" id="mobile_vidbg" autoplay="autoplay"></video>
-		</div>
+
 
 
 		<?php
