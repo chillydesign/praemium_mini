26b9f470200c0b24b5626734724d7ec33139cbc5
Lil <rissel.melissa@gmail.com>
2022-06-21 11:37:48 +0100

    test put gls on home page

--- template-home.php
+++ template-home.php
@@ -1,4 +1,19 @@
 <?php /* Template Name: Home Page Template */ ?>
-<script type="text/javascript">
-	window.location.href='http://praemium.ch';
+
+<div id='reciprocity'></div>
+<script src='https://praemium.luxuryrealestate.com/reciprocity.js' type='text/javascript'></script>
+<script type='text/javascript'>
+	Reciprocity.init({
+		membersite: "praemium",
+		parentLocation: window.location
+	});
 </script>
+
+
+
+
+<!-- Old version redirected to praemium website -->
+<script type="text/javascript">
+	window.location.href = 'http://praemium.ch';
+</script> -->
+<!-- End of old version with redirection -->
