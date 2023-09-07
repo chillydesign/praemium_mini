1efbcc93a468960a8022997076f440fa04689c78
Charles Harvey <harvey.charles@gmail.com>
2023-09-07 10:23:46 +0100

    remover ['browser_name_pattern'] in functions praemium

--- index.php
+++ index.php
@@ -5,19 +5,19 @@
 
 
 
-			<article id="main_article">
+<article id="main_article">
 
-			<?php if( get_option('page_for_posts' ) ) : ?>
-			<h1 ><?php echo apply_filters('the_title',get_page( get_option('page_for_posts') )->post_title); ?></h1>
-			<?php endif; ?>
+	<?php if (get_option('page_for_posts')) : ?>
+		<h1><?php echo apply_filters('the_title', get_page(get_option('page_for_posts'))->post_title); ?></h1>
+	<?php endif; ?>
 
 
 
-				<?php get_template_part('loop'); ?>
+	<?php get_template_part('loop'); ?>
 
-				<?php get_template_part('pagination'); ?>
-			</article>
-	
+	<?php get_template_part('pagination'); ?>
+</article>
+
 
 
 <?php get_template_part('footer_html'); ?>
